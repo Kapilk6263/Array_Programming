7a690926519ce26fdc2a281a4a0dea050484a182
Kapilk6263 <Kapilkapil6465@gmail.com>
2025-11-15 13:10:14 +0530

    Move the element of array right by one

--- MoveElementLeft.java
+++ MoveElementLeft.java
@@ -9,6 +9,17 @@ public class MoveElementLeft {
            arr[i-1]=arr[i];
         }
         arr[n-1]=temp;
-        System.out.println(Arrays.toString(arr));
-    }
+        System.out.println(Arrays.toString(arr)); 
+
+        int arr2[]={3,4,5,8,9};
+        int n1=arr2.length;
+        int temp2=arr2[n1-1];
+
+        for(int j=n1-1;j>0;j--){
+           arr2[j]=arr2[j-1];
+        }
+        arr2[0]=temp2;
+        System.out.println(Arrays.toString(arr2));
+        
+}
 }
