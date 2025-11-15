b19909b443a7ba29913e796f557de9a04448edc9
Kapilk6263 <Kapilkapil6465@gmail.com>
2025-11-15 15:07:01 +0530

    Move all the zeros to last

--- ZerosToEnd.java
+++ ZerosToEnd.java
@@ -1,26 +1,32 @@
+import java.util.Arrays;
+
 public class ZerosToEnd {
     public static void main(String[] args) {
         int []arr={5,6,0,7,5,0,0,8};
+        int n=arr.length;
 
-        int z=0;
-        int i=0;
-        while(i<arr.length){
-            if(arr[i]==0){
-                z=i;
-               break;
-            }
-            i++;
-        } 
-        for(int nz=z+1;nz<arr.length;nz++){
-           if (arr[nz] != 0) {
-                int temp = arr[z];
-                arr[z] = arr[nz];
-                arr[nz] = temp;
-                z++;
-            }
-        }
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
+       int j=-1;
+       for(int i=0;i<n;i++){
+           if(arr[i]==0) {
+            j=i;
+             break;
+           }
+          
+       }  
+
+       for(int i=j+1;i<n;i++){
+         if(arr[i]!=0){
+           swap(arr, i, j);
+            j++;
+         }
+
+       } 
+       System.out.println(Arrays.toString(arr));
     }
-}
+
+    static void swap(int []arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}  
