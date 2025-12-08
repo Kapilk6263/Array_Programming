85d0afc880ab1f0848ce951566d438756eef1ade
Kapilk6263 <Kapilkapil6465@gmail.com>
2025-12-08 11:42:43 +0530

    update

--- MaximumConsecutiveOne.java
+++ MaximumConsecutiveOne.java
@@ -10,7 +10,7 @@ public class MaximumConsecutiveOne {
         for(int i=0;i<n;i++){
             if(arr[i]==1){
                 count++;
-                max=Math.max(max, count);
+                max=count;
             }else {
                 count=0;
             }
--- SumOfMaximumSubArray.java
+++ SumOfMaximumSubArray.java
@@ -1,27 +1,31 @@
 public class SumOfMaximumSubArray {
     public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 4, 5 };
+       // int arr[] = { 1, 2, 3, 4, 5 };
+      
 
-        // int max_sum = 0;
-        // int sum = 0;
+    //     int max_sum =Integer.MIN_VALUE ;
+    //     int sum = 0;
 
-        // for (int i = 0; i < arr.length; i++) {
+    //      for (int i = 0; i < arr.length; i++) {
 
-        //     sum = 0;
-        //     for (int j = i; j < arr.length; j++) {
-        //         sum += arr[j];
-        //     }
+    //         sum = 0;
+    //         for (int j = i; j < arr.length; j++) {
+    //             sum += arr[j];
+    //         }
 
-        //     System.out.println();
-        //     if (max_sum < sum) {
-        //         max_sum = sum;
-        //     }
+    //         System.out.println();
+    //         if (max_sum < sum) {
+    //             max_sum = sum;
+    //         }
+    //     }
 
-        //     System.out.println(max_sum);
+    //         System.out.println(max_sum);
 
-        //}
+        
+    // }}
 
 
+       int arr[] = { 1, 2, -3, 4, -5,4,6 };
        int sum=0;
        int max_sum=Integer.MIN_VALUE;
 
